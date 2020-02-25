6b5037db190e39a351e47de16b953f3c99a251dc
Ilnur Saifutdinov <ilnursoft@gmail.com>
2020-02-25 22:15:08 +0300

    создание скрипта отправки нотификаций клиентам

---
+++ telegram_notify.py
@@ -0,0 +1,23 @@
+"""
+    отправка нотификации клиенту используя телеграмм бота
+"""
+
+class SendNotify:
+
+    # инициализация
+    def __init__(self):
+        pass
+
+    # добавление клиента которому нужно отправлять нотификации
+    def add_clent(self):
+        pass
+
+    # отправка всем клиентам сообщения msg
+    def send_msg(self, msg):
+        pass
+
+
+if __name__ == "__main__":
+    tlg = SendNotify()
+    print(tlg)
+
