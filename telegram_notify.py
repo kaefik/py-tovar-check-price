13f33c18aca66cfd37ec3225f82c4aa26fb07427
Ilnur Saifutdinov <ilnursoft@gmail.com>
2020-02-26 23:16:09 +0300

    add example tlg bot

--- telegram_notify.py
+++ telegram_notify.py
@@ -1,12 +1,42 @@
 """
     отправка нотификации клиенту используя телеграмм бота
+
+    использовать будем https://github.com/LonamiWebs/Telethon
 """
+
+import os
+from dotenv import load_dotenv
+from telethon import TelegramClient, events, sync
+
+
 
 class SendNotify:
 
     # инициализация
     def __init__(self):
-        pass
+        dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+        print((dotenv_path))
+        if os.path.exists(dotenv_path):
+            load_dotenv(dotenv_path)
+        self.api_id = os.getenv("TLG_API_ID")
+        self.api_hash = os.getenv("TLG_API_HASH")
+        client = TelegramClient('send_notify-kaefik', self.api_id, self.api_hash)
+        client.start()
+        """
+
+        print(client.get_me().stringify())
+
+        client.send_message('username', 'Hello! Talking to you from Telethon')
+        client.send_file('username', '/home/myself/Pictures/holidays.jpg')
+
+        client.download_profile_photo('me')
+        messages = client.get_messages('username')
+        messages[0].download_media()
+
+        @client.on(events.NewMessage(pattern='(?i)hi|hello'))
+        async def handler(event):
+            await event.respond('Hey!')
+        """
 
     # добавление клиента которому нужно отправлять нотификации
     def add_clent(self):
@@ -18,6 +48,6 @@ class SendNotify:
 
 
 if __name__ == "__main__":
-    tlg = SendNotify()
-    print(tlg)
 
+    tlg = SendNotify()
+    print(tlg.api_id)
